f1685d201bda9bc2a58cb60d6700f183c832c5ab
dgrasman <dgrasman@usfca.edu>
2026-09-18 20:54:20 -0700

    Update Shore Power chart to use percentage on secondary y-axis

--- streamlit_app.py
+++ streamlit_app.py
@@ -272,7 +272,7 @@ with tab2:
             # Calculate the absolute number of plugged-in vessels
             df_act["PluggedInVessels"] = df_act["VesselCalls"] * (df_act["ShorePowerPluginRate"] / 100)
             
-            fig_shore = make_subplots(specs=[[{"secondary_y": False}]])
+            fig_shore = make_subplots(specs=[[{"secondary_y": True}]])
             
             fig_shore.add_trace(
                 go.Bar(
@@ -281,23 +281,26 @@ with tab2:
                     name="Total Vessel Calls",
                     marker_color="#cbd5e1"
                 ),
+                secondary_y=False,
             )
             
             fig_shore.add_trace(
                 go.Scatter(
                     x=df_act["Year"], 
-                    y=df_act["PluggedInVessels"], 
-                    name="Plugged-In Vessels",
+                    y=df_act["ShorePowerPluginRate"], 
+                    name="Plug-in Rate (%)",
                     mode='lines+markers',
                     line=dict(color="#10b981", width=3)
                 ),
+                secondary_y=True,
             )
             
             fig_shore.update_layout(
                 hovermode="x unified",
                 barmode='group'
             )
-            fig_shore.update_yaxes(title_text="Vessels")
+            fig_shore.update_yaxes(title_text="Total Vessel Calls", secondary_y=False)
+            fig_shore.update_yaxes(title_text="Plug-in Rate (%)", range=[0, 100], secondary_y=True)
             
             fig_shore = apply_clean_layout(fig_shore)
             st.plotly_chart(fig_shore, use_container_width=True)
